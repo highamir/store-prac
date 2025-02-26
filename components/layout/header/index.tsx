adf03f9710a60cc5d207316971bdfed28a533311
highamir <amiraghebati24@gmail.com>
2025-02-26 12:49:59 +0330

    feat: add mainheader component

---
+++ components/layout/header/index.tsx
@@ -0,0 +1,82 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { Icon } from "@iconify-icon/react";
+import { usePathname } from "next/navigation";
+import { Button } from "@heroui/react";
+
+
+const links = [
+  { label: "HOME", url: "/" },
+  { label: "TEMPLATE", url: "/template" },
+  { label: "DOCS", url: "/docs" },
+  { label: "SALE", url: "/sale" },
+];
+
+export default function MainHeader() {
+  const pathname = usePathname();
+  console.log(pathname);
+  return (
+    <nav className="flex h-16 bg-red-50 w-full items-center  justify-between p-7">
+      <div className="flex w-8  items-center ">
+        <ul className="flex flex-row gap-6 items-center">
+          {links.map((item, index) => (
+            <li
+              className={
+                pathname == item.url
+                  ? "text-red-700 font-medium hover:text-pink-200"
+                  : "text-black font-medium  hover:text-pink-200"
+              }
+              key={index}
+            >
+              <Link href={item.url}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <h1 className="text-black font-bold justify-self-center">SHOPY</h1>
+      <div className="flex  gap-3 ">
+        <Button isIconOnly variant="light">
+          <Icon
+            icon="lucide:search"
+            width="24"
+            height="24"
+            style={{ color: "#878787" }}
+          />
+        </Button>
+        <Button isIconOnly variant="light">
+          <Icon
+            icon="mingcute:earth-line"
+            width="24"
+            height="24"
+            style={{ color: "#878787" }}
+          />
+        </Button>
+        <Button isIconOnly variant="light">
+          <Icon
+            icon="iconamoon:profile-duotone"
+            width="24"
+            height="24"
+            style={{ color: "#878787" }}
+          />
+        </Button>
+        <Button isIconOnly variant="light">
+          <Icon
+            icon="mdi:heart-outline"
+            width="24"
+            height="24"
+            style={{ color: "#878787" }}
+          />
+        </Button>
+        <Button isIconOnly variant="light">
+          <Icon
+            icon="solar:cart-linear"
+            width="24"
+            height="24"
+            style={{ color: "#878787" }}
+          />
+        </Button>
+      </div>
+    </nav>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,17 @@
 import type { Config } from "tailwindcss";
+const { heroui } = require("@heroui/react");
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
 export default config;
